c0dfa09ec27f14865a6b80774e89b7b7c42a2504
Vin <purrup27@gmail.com>
2021-03-08 22:01:57 +0800

    Modify design of tweets list

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,108 +1,109 @@
 <template>
-  <q-page>
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
-      <div class="col">
-        <q-input
-          v-model="newQwteetContent"
-          class="new-qweet"
-          placeholder="What's happening"
-          maxlength="280"
-          counter
-          autogrow
-          >
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar5.jpg">
-            </q-avatar>
-          </template>
-        </q-input>
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            v-model="newQwteetContent"
+            class="new-qweet"
+            placeholder="What's happening"
+            maxlength="280"
+            counter
+            autogrow
+            >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewQweet"
+            class="q-mb-lg"
+            :disable="!newQwteetContent"
+            color="primary"
+            label="Qweet"
+            rounded
+            unelevated
+            no-caps
+            ></q-btn>
+        </div>
       </div>
-      <div class="col col-shrink">
-        <q-btn
-          @click="addNewQweet"
-          class="q-mb-lg"
-          :disable="!newQwteetContent"
-          color="primary"
-          label="Qweet"
-          rounded
-          unelevated
-          no-caps
-          ></q-btn>
-      </div>
-    </div>
 
-    <q-separator
-      class="divider"
-      size="10px"
-      color="grey-2"/>
+      <q-separator
+        class="divider"
+        size="10px"
+        color="grey-2"/>
 
-    <q-list separator>
-      <transition-group
-        appear
-        enter-active-class="animated fadeIn slow"
-        leave-active-class="animated fadeOut slow"
-      >
-        <q-item
-        v-for="qweet in qweets"
-        :key="qweet.id"
-        class="q-py-md qweet-separator">
-          <q-item-section avatar top>
-            <q-avatar size="xl">
-              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-            </q-avatar>
-          </q-item-section>
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn slow"
+          leave-active-class="animated fadeOut slow"
+        >
+          <q-item
+          v-for="qweet in qweets"
+          :key="qweet.id"
+          class="q-py-md qweet-separator">
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label class="text-subtitle1">
-              <strong>
-                Vin
-              </strong>
-              <span class="text-grey-7">
-                @purrup
-              </span>
-            </q-item-label>
-            <q-item-label class="qweet-content">
-              {{ qweet.content }}
-            </q-item-label>
-            <div class="qweet-icons row justify-between q-mt-sm">
-              <q-btn
-                color="grey"
-                size="sm"
-                icon="far fa-comment"
-                flat
-                round
-              />
-              <q-btn
-                color="grey"
-                size="sm"
-                icon="fas fa-retweet"
-                flat
-                round
-              />
-              <q-btn
-                color="grey"
-                size="sm"
-                icon="far fa-heart"
-                flat
-                round
-              />
-              <q-btn
-                @click="deleteQweet(qweet.id)"
-                color="grey"
-                size="sm"
-                icon="fas fa-trash"
-                flat
-                round
-              />
-            </div>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-subtitle1">
+                <strong>
+                  Vin
+                </strong>
+                <span class="text-grey-7">
+                  @purrup
+                  <br class="lt-md">&bull; {{ qweet.date |  relativeDate }}
+                </span>
+              </q-item-label>
+              <q-item-label class="qweet-content">
+                {{ qweet.content }}
+              </q-item-label>
+              <div class="qweet-icons row justify-between q-mt-sm">
+                <q-btn
+                  color="grey"
+                  size="sm"
+                  icon="far fa-comment"
+                  flat
+                  round
+                />
+                <q-btn
+                  color="grey"
+                  size="sm"
+                  icon="fas fa-retweet"
+                  flat
+                  round
+                />
+                <q-btn
+                  color="grey"
+                  size="sm"
+                  icon="far fa-heart"
+                  flat
+                  round
+                />
+                <q-btn
+                  @click="deleteQweet(qweet.id)"
+                  color="grey"
+                  size="sm"
+                  icon="fas fa-trash"
+                  flat
+                  round
+                />
+              </div>
+            </q-item-section>
+          </q-item>
+        </transition-group>
+      </q-list>
 
-          <q-item-section side top>
-            {{ qweet.date |  relativeDate }}
-          </q-item-section>
-        </q-item>
-      </transition-group>
-    </q-list>
+    </q-scroll-area>
   </q-page>
 </template>
 
@@ -123,6 +124,26 @@ export default {
           id: this.uuid(),
           content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magni repellendus! Repellendus non ipsa, laudantium omnis sunt, repellat dicta voluptas cum provident repudiandae architecto inventore. Reprehenderit error ducimus officiis non',
           date: 1615208202055
+        },
+        {
+          id: this.uuid(),
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magni repellendus! Repellendus non ipsa, laudantium omnis sunt, repellat dicta voluptas cum provident repudiandae architecto inventore. Reprehenderit error ducimus officiis non',
+          date: 1615208104428
+        },
+        {
+          id: this.uuid(),
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magni repellendus! Repellendus non ipsa, laudantium omnis sunt, repellat dicta voluptas cum provident repudiandae architecto inventore. Reprehenderit error ducimus officiis non',
+          date: 1615208104428
+        },
+        {
+          id: this.uuid(),
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magni repellendus! Repellendus non ipsa, laudantium omnis sunt, repellat dicta voluptas cum provident repudiandae architecto inventore. Reprehenderit error ducimus officiis non',
+          date: 1615208104428
+        },
+        {
+          id: this.uuid(),
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magni repellendus! Repellendus non ipsa, laudantium omnis sunt, repellat dicta voluptas cum provident repudiandae architecto inventore. Reprehenderit error ducimus officiis non',
+          date: 1615208104428
         }
       ]
     }

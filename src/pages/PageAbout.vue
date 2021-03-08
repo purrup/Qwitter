f7881ec9ffd552cdb83b7bdaa003ab0d27de3bef
Vin <purrup27@gmail.com>
2021-03-08 17:26:36 +0800

    Create about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page class="">
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body-1">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis at blanditiis cupiditate sit autem eveniet natus quo, necessitatibus delectus consequuntur tenetur animi. Nesciunt veniam accusamus tempora nulla ea, laudantium amet!</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis at blanditiis cupiditate sit autem eveniet natus quo, necessitatibus delectus consequuntur tenetur animi. Nesciunt veniam accusamus tempora nulla ea, laudantium amet!</p>
+    </div>
   </q-page>
 </template>
 

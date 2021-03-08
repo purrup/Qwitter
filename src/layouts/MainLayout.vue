7ba1470d2a4b17078b095a0c2b3718153a906257
Vin <purrup27@gmail.com>
2021-03-08 15:55:45 +0800

    Create layout, header, left drawer and right drawer

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,116 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Qwitter
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered :width="283">
+      <q-icon
+      color="primary"
+      class="q-pa-md"
+      name="fab fa-twitter"
+      size="lg"
+       />
+      <q-list>
+        <q-item
+        to="/"
+        clickable
+        v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon  name="home" size="md"/>
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item
+        to="/about"
+        clickable
+        v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="help" size="md"/>
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <q-input
+      placeholder="Search Qwitter"
+      class="q-ma-md"
+      rounded
+      outlined
+      dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+      <q-list
+      padding
+      separator
+      >
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold">Something amazing happened</q-item-label>
+            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>

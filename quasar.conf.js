e8da618b131281b5b6825194f1ad743f41143a55
Vin <purrup27@gmail.com>
2021-03-10 01:10:04 +0800

    Revert deploy to windows

--- quasar.conf.js
+++ quasar.conf.js
@@ -192,7 +192,7 @@ module.exports = function (/* ctx */) {
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
       nodeIntegration: true,
-      enableRemoteModule: true,
+
       extendWebpack (/* cfg */) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
